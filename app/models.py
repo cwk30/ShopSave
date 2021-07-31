da3a8c1659269c4033cd02840f18f191a1d76704
cwk30 <james.rokz.007@gmail.com>
2021-07-31 15:37:36 +0800

    dummy users and tables created

--- app/models.py
+++ app/models.py
@@ -6,17 +6,35 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+class Voucher(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), nullable=False)
+    cashiername = db.Column(db.String(20), nullable=False)
+    expiry = db.Column(db.Integer, nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    transfer = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+
+class VoucherCat(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    value = db.Column(db.Integer, nullable=False)
+    transfer = db.Column(db.Integer, nullable=False)
+    cost = db.Column(db.Integer, nullable=False)
+    expirydur = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    cashiername = db.Column(db.String(20), unique=True, nullable=False)
+    sold = db.Column(db.Integer, nullable=False)
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True) 
     username = db.Column(db.String(20), unique=True, nullable=False) 
-    #name = db.Column(db.String(20), unique=True, nullable=False) 
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     cashier = db.Column(db.Integer, nullable=False)
-    #contactno = db.Column(db.Integer)
-    #dateofbirth
-    #photo
-    #address
+    contactno = db.Column(db.Integer)
+    dateofbirth = db.Column(db.Integer)
+    photo = db.Column(db.String)
+    address = db.Column(db.String)
 
     #def get_reset_token(self, expires_sec=1800):
     #    s = Serializer(app.config['SECRET_KEY'], expires_sec)
